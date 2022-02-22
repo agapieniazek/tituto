65277d431f5007aa0ce745a2b00d0b5405445849
Aga <dzieszkol@gmail.com>
2022-02-22 15:34:55 +0100

    finished about and offer section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,14 @@
+import "../style/about.scss";
+import img from "../images/about.jpg";
+
 const About = () => {
   return ( 
-    <div>
-      About
+    <div className="about" id="about">
+      <img src={img} alt="coffee"></img>
+      <div className="about__info">
+        <h2>O nas</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odit vellpa! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit explicabo aliquid hic ea id, perferendis quas nesciunt ipsam corrupti tempore dignissimos placeat sint quis asperiores alias modi laudantium amet sapiente. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, aut! Magni beatae ipsam suscipit officiis, laudantium repudiandae molestias totam aut aliquid cum delectus autem nisi molestiae, dolorum tempore nemo non! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, accusamus officiis. Id iure possimus vitae, temporibus doloremque quos tempora nulla voluptas voluptatibus saepe necessitatibus vero sapiente fuga libero assumenda adipisci?</p>
+      </div>
     </div>
    );
 }
--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,7 +1,44 @@
+import "../style/offer.scss";
+import img1 from "../images/coffee-g162304a9d_1920.jpg";
+import img2 from "../images/coffe-grains-gbcc72b9db_1920.jpg";
+import img3 from "../images/pancakes-g050b113c5_1920.jpg";
+
+
+const list= [
+  {
+    img: img1,
+    title: "Wyśmienite kawy"
+  },
+  {
+    img: img2,
+    title: "Ziarna na wynos"
+  },
+   {
+    img: img3,
+    title: "Ciasta jak domowe"
+  }
+]
+
 const Offer = () => {
+  const offerList = list.map((offer, index)=>{
+    return (
+      <li key={index} className="offer__list__item">
+        <img src={offer.img} alt="offer"/>
+        <h3>{offer.title}</h3>
+      </li>
+    )    
+  })
+
   return ( 
-    <div>
-      Offer
+    <div className="offer" id="offer">
+      <div className="offer__title">
+        <h2>Oferta</h2>
+      </div>
+      <div className="offer__list">
+        {offerList}
+      </div>
+
+      
     </div>
    );
 }
