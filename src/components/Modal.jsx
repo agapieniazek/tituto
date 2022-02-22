b50823195e981baee94c01413c20d2ce3a0be425
Aga <dzieszkol@gmail.com>
2022-02-22 18:02:53 +0100

    RWD

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,16 @@
+import { Link } from "react-scroll";
+
 const Modal = ({clickedImg, handleRotationRight, handelRotationLeft, setClickedImg}) => {
 
   const handleClick =(e)=>{
     if(e.target.classList.contains("dismiss")){
       setClickedImg(null);
-      window.scrollTo(0, 1500);
     }
   }
   return ( 
     <div className="overlay">
       <img src={clickedImg} alt="" className="overlay__img" />
-      <span className="dismiss overlay__close" onClick={handleClick}>X</span>
+      <Link to="gallery"><span className="dismiss overlay__close" onClick={handleClick}>X</span></Link>
       <div onClick={handelRotationLeft} className="overlay__arrows overlay__arrows__left">
       <i className="fas fa-chevron-left"></i>
       </div>
