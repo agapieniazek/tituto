fb4bf31a199c4eff8d37c9b254527330bf5c4dee
Aga <dzieszkol@gmail.com>
2022-02-22 17:13:07 +0100

    smooth scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import "../style/navbar.scss";
+import {Link} from "react-scroll";
 
 const Navbar = () => {
   return ( 
     <div className="navbar">
       <ul className="navbar__list">
-        <li className="navbar__link"><a href="/">O nas</a></li>
-        <li className="navbar__link"><a href=""> Oferta</a></li>
+        <li className="navbar__link"><Link to="about" smooth={true} spy={true} offset={-50} duration={500}>O nas</Link></li>
+        <li className="navbar__link"><Link to="offer" smooth={true} spy={true} offset={-50} duration={500}> Oferta</Link></li>
         </ul>
         <ul className="navbar__list">
-        <li className="navbar__link"><a href="">Galeria</a></li>
-        <li className="navbar__link"><a href="">Kontakt</a></li>
+        <li className="navbar__link"><Link to="gallery" smooth={true} spy={true} offset={-50} duration={500}>Galeria</Link></li>
+        <li className="navbar__link"><Link to="footer" smooth={true} spy={true} offset={-50} duration={500}>Kontakt</Link></li>
       </ul>
     </div>
    );
